4ba2063eb4d8d29a3fd8f378f95c9b3009e6a766
iTsThysis <wangdarwin5@gmail.com>
2022-06-29 00:52:38 -0500

    first commit

---
+++ src/VoltageDivision.ino
@@ -0,0 +1,43 @@
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+#define lightSensor A5
+#define LED D5
+#define signal D4
+
+int maxValue = 0;
+int minValue = 0;
+int timer = 0;
+
+void setup() {
+  pinMode(lightSensor, INPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(signal, OUTPUT);
+  Serial.begin(9600);
+  Serial.println(timer);
+}
+
+
+void loop() {
+  
+  int value = analogRead(lightSensor);
+  maxValue = max(maxValue, value);
+  minValue = min(minValue, value);
+  
+  int brightness = map(value, minValue, maxValue, 0, 255);
+  
+  analogWrite(LED, brightness);
+
+  if (timer == 25) {
+    maxValue = 0;
+    minValue = 0;
+    timer = 0;
+    digitalWrite(signal, HIGH);
+    delay(200);
+  } else {
+    timer = timer + 1;
+    digitalWrite(signal, LOW);
+    delay(192);
+  }
+  
+}
